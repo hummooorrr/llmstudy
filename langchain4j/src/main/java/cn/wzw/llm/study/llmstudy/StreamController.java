5863d9b548003a995e520f83b04a059d13db65b4
wangziwei21 <wangziwei21@gaotu.cn>
2026-04-14 18:20:13 +0800

    langchain4j基本使用

---
+++ langchain4j/src/main/java/cn/wzw/llm/study/llmstudy/StreamController.java
@@ -0,0 +1,84 @@
+package cn.wzw.llm.study.llmstudy;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/streamcontroller")
+public class StreamController {
+
+    @Autowired
+    OpenAiStreamingChatModel streamingChatModel;
+
+    @Autowired
+    OpenAiChatModel chatModel;
+
+    @RequestMapping("/streamHello")
+    public Flux<String> streamHello(HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        Flux<String> flux = Flux.create(fluxSink -> {
+            streamingChatModel.chat("你好,你是谁？", new StreamingChatResponseHandler() {
+                @Override
+                public void onPartialResponse(String partialResponse) {
+                    fluxSink.next(partialResponse);
+                }
+
+                @Override
+                public void onCompleteResponse(ChatResponse completeResponse) {
+                    fluxSink.complete();
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    fluxSink.error(error);
+                }
+            });
+        });
+        return flux;
+    }
+
+    @RequestMapping("/memory")
+    public String memory(HttpServletResponse response) {
+        List<ChatMessage> messages = new ArrayList<>();
+
+        //第一轮对话
+        messages.add(SystemMessage.systemMessage("你是一个AI助手"));
+        messages.add(UserMessage.userMessage("我叫Hollis，是一个程序员"));
+        AiMessage answer = chatModel.chat(messages).aiMessage();
+        System.out.println(answer);
+        System.out.println("======");
+
+        messages.add(answer);
+
+        //第二轮对话
+        messages.add(UserMessage.userMessage("Hollis是干什么的?"));
+        AiMessage answer1 = chatModel.chat(messages).aiMessage();
+        System.out.println(answer1);
+        System.out.println("======");
+
+        messages.add(answer1);
+
+        //第三轮对话
+        messages.add(UserMessage.userMessage("我是谁？"));
+        AiMessage answer2 = chatModel.chat(messages).aiMessage();
+        System.out.println(answer2);
+        System.out.println("======");
+
+        return answer2.text();
+    }
+}
