5a10fc450ed60c87c4ef0f076575513ee10398ad
wangziwei21 <wangziwei21@gaotu.cn>
2026-04-15 11:31:36 +0800

    functioncall基本使用

---
+++ functioncall/src/main/java/cn/wzw/llm/study/llmstudy/FunctionCall.java
@@ -0,0 +1,13 @@
+package cn.wzw.llm.study.llmstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionCall {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionCall.class, args);
+	}
+
+}
